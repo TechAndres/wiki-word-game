718301326aa2b11d2fa4c01c8ebb4dc577c12b60
Andres Valencia <carlos.valencia10@outlook.com>
2018-09-25 03:13:14 -0500

    finish the table game animation

--- js/main.js
+++ js/main.js
@@ -1,28 +1,72 @@
+function startMouseover(){
+  $("#start-button").mouseover(function(){
+        $(this).html("ready");
+  $(this).css({"background-color":"#BDE517",border:"5px solid #D1FF17"});
+  $(this).click(function(){ $(this).css({border:"5px solid #D1FF17"})});
+});
+}
+
+
+function startMouseOut(){
+  $("#start-button").mouseout(function(){
+          $(this).html("start");
+          $(this).css({"background-color":"#FFB404",border:"5px solid #FFB404"});
+
+      });
+}
+
+function clickButtonStart(){
+  $("#start-button").click(function(){
+      $("#start").fadeTo(1500, 0 ,function(){
+         $("#start").hide();
+         $("#start-count").css("display","block");
+         $("#start-count").prepend("<img id='theImg' src='./images/go.gif'/>");
+         setTimeout(function() {
+              $("#theImg").hide();
+              $("#start-count").hide();
+              $("#board-game").show();
+              setBoardGame();
+            },
+              2100);
+       });
+    })
+}
+
+function clickButtonBack(){
+  $("#back-button").click(function(){
+               $("#start").css("opacity","1");
+               $("#start").show();
+                 $("#board-game").css("display","none");
+ });
+}
+
+function setBoardGame(){
+  $("#board-image").animate( {"top":"10px"},function(){
+      setTimeout(function(){
+        $("#board-image").css({"transform":"rotate(20deg)"});
+        $("#board-image").css({"transition":"all 1s"});
+        $("#board-image").animate({"top":"-280px"});
+        setTimeout(
+            function()
+            {
+              $("#board-image").css({"transform":"rotate(-12deg)"});
+              setTimeout(
+                    function()
+                    {
+                      $("#board-image").css({"transform":"rotate(-0)"});
+                    }, 320);
+            }, 320);
+      } ,450)
+      }
+    );
+}
+
+
+
 $(document).ready(function(){
-          $("#start-button").mouseover(function(){
-                $(this).html("ready");
-          $(this).css({"background-color":"#D1FF17",border:"5px solid #D1FF17"});
+            startMouseover();
+            startMouseOut();
+            clickButtonStart();
+            clickButtonBack();
 
-          });
-
-          $("#start-button").mouseout(function(){
-                  $(this).html("start");
-                  $(this).css({"background-color":"#FFB404",border:"5px solid #FFB404"});
-
-              });
-
-          $("#start-button").click(function(){
-              $("#start").fadeTo(1500, 0 ,function(){
-                 $("#start").css("display","none");
-                 $("#full-game").css("display","block");
-                 $("#full-game").prepend("<img id='theImg' src='./images/go.gif'/>");
-                 setTimeout(function() { $("#theImg").hide(1000)}, 2100);
-               });
-
-           $("#back-button").click(function(){
-                        $("#start").css("display","block");
-                        $("#start").css("opacity","1");
-                          $("#full-game").css("display","none");
-          });
-        })
-  })
+})
