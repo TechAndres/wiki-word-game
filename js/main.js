2461464e1a38037b7a2030d63f47b7c9056589e4
Andres Valencia <carlos.valencia10@outlook.com>
2018-10-11 14:09:38 -0500

    finish today

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 var word =[];
+var score = 0;
+var wordok= [];
+
 $("html").mousedown(function(e){ e.preventDefault(); });
     // word and prevent the select of the div
 
       // begining of the animation start when the arrow it on the button
 function startMouseover(){
   $("#start-button").mouseover(function(){
-        $(this).html("ready");
+        $(this).html("READY");
         $(this).css({"background-color":"#BDE517",border:"5px solid #D1FF17"});
         $(this).click(function(){ $(this).css({border:"5px solid #D1FF17"})});
         setTimeout(function(){
@@ -21,7 +24,7 @@ function startMouseover(){
       // begining of the animation start when the arrow it off the button
 function startMouseOut(){
   $("#start-button").mouseout(function(){
-          $(this).html("start");
+          $(this).html("COMENZAR");
           $(this).css({"background-color":"#FFB404",border:"5px solid #FFB404"});
       });
 }
@@ -94,6 +97,9 @@ function  boardGameAnimate(){
 }
 
 function stageGameAnimate(){
+      $("#score").hide().fadeIn(290);
+      $("#boy").hide().fadeIn(290);
+      $(".answers-num").hide().fadeIn(290);
       $("#screen-letter").animate({"bottom":"14%", "left": "33%"});
 
       $("#girl-stage").animate({"bottom":"120px"});
@@ -103,32 +109,70 @@ function stageGameAnimate(){
       $("#mix-b").animate({"bottom":"22%"});
 }
 
-
-function clickButtonOk(){
-  $("#ok-b").click(function(){
-    var wordjoin = word.join();
-    var answers = ["A1,M,A,R,T,E","A,M,A1,R","A1,M,A,R","R,A,M,A1","A,M,A1,R,T,E","R,A1,M,A","A1,M,A","A,M,A1"];
-    answers.forEach(function(rightw){
-      if (wordjoin===rightw){
-        $("#board-match").append(rightw);
-        console.log('yes')
-      }
-    });
- });
-}
-
 function remove(array, element) {
    const index = array.indexOf(element);
    array.splice(index, 1);
 }
 
+ function clickButtonOk(){
+
+  $("#ok-b").click(function(){
+    var wordjoin = word.join();
+    wordjoin2 = wordjoin;
+    var answers = [`A1,M,A,R,T,E`,`A,M,A1,R`,`A1,M,A,R`,`R,A,M,A1`,`A,M,A1,R,T,E`,`R,A1,M,A`,`A1,M,A`,`A,M,A1`,`M,A,T,E,R,A1`,`M,A1,T,E,R,A`,`M,A1,T,A`,`M,A,T,A1`,`M,E,T,A1`,`M,E,T,A`,`M,A,T,A1,R`,`M,A1,T,A,R`,
+    `R,E,M,A`,`R,E,M,A1`,`T,E,M,A`,`T,E,M,A1`,`R,A,T,A`,`R,A,T,A1`,`R,A1,T,A`,`T,A1,R,M,A`,`T,R.A,M,A1`,`T,R,A1,M,A`,`T,R,A,M,A1`,`M,A1,R,T,E`,`M,A,R,T,E`,`M,A,R,E,A`,`M,A,R,E,A1`];
+
+    answers.forEach(function(rightw){
+      if (wordjoin===rightw){
+
+        var res = wordjoin2.replace(/,/g, "-");
+        score= score + 5 ;
+        $("#score").text(score);
+        console.log(score);
+        $(`.${res}`).css("color","white");
+        $(`.${res}`).css("background-color","rgb(123, 191, 63)");
+
+        $("#board-game").css("background-color","rgba(123, 191, 63, 0.5)");
+        setTimeout(
+              function()
+              {
+                $("#board-game").css("background-color","transparent");
+              }, 270);
+        console.log('yes');
+        console.log(answers);
+        remove(answers,rightw);
+      }
+      else{
+        setTimeout(
+              function()
+              {
+
+                $("#background-game").css("background-color","transparent");
+              }, 270);
+
+      }
+    });
+ });
+}
+
+
+function deleteB(){
+  $("#delete-b").click(function(){
+    $("#letters-up").empty();
+    $("#letters").empty();
+     word = [] ;
+     gameMatch1();
+
+});
+}
+
 function gameMatch1(){
-   var letter_A = $("#letters").prepend("<img id='A' src='./images/A.png'/>").hide().fadeIn(1200);
-    var letter_R = $("#letters").prepend("<img id='R' src='./images/R.png'/>").hide().fadeIn(1200);
-    var letter_M = $("#letters").prepend("<img id='M' src='./images/M.png'/>").hide().fadeIn(1200);
-    var letter_T = $("#letters").prepend("<img id='T' src='./images/T.png'/>").hide().fadeIn(1200);
-    var letter_E = $("#letters").prepend("<img id='E' src='./images/E.png'/>").hide().fadeIn(1200);
-    var letter_A1 = $("#letters").prepend("<img id='A1' src='./images/A.png'/>").hide().fadeIn(1200);
+   var letter_A = $("#letters").prepend("<img id='A' src='./images/A.png'/>").hide().fadeIn(120);
+    var letter_R = $("#letters").prepend("<img id='R' src='./images/R.png'/>").hide().fadeIn(120);
+    var letter_M = $("#letters").prepend("<img id='M' src='./images/M.png'/>").hide().fadeIn(120);
+    var letter_T = $("#letters").prepend("<img id='T' src='./images/T.png'/>").hide().fadeIn(120);
+    var letter_E = $("#letters").prepend("<img id='E' src='./images/E.png'/>").hide().fadeIn(120);
+    var letter_A1 = $("#letters").prepend("<img id='A1' src='./images/A.png'/>").hide().fadeIn(120);
 
 
     function upLetter(id){
@@ -165,14 +209,6 @@ function gameMatch1(){
 
 }
 
-function deleteB(){
-  $("#delete-b").click(function(){
-    let pop = word.pop();
-    let x1 = $(`#${pop}`).detach();
-     $("#letters").append(x1);
-
-});
-}
 
 function arrayAnswer(compare){
     answers = ["amarte","amar"];
